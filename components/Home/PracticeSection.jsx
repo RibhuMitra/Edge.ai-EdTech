d88b9fac989b9576bc7d0b7487b64bcd950db9b2
RibhuMitra <ribhumitra30@gmail.com>
2025-04-05 13:45:00 +0530

    Practice Section Update

--- components/Home/PracticeSection.jsx
+++ components/Home/PracticeSection.jsx
@@ -1,39 +1,55 @@
 import { View, Text, FlatList, Image } from 'react-native'
-import React, { Component } from 'react';
-import { PracticeOption } from '../../constant/Option'
+import React from 'react';
 
+export const PraticeOption = [
+  {
+    name: 'Quiz',
+    image: require('./../../assets/images/quizz.png'),
+    icon: require('./../../assets/images/quiz.png'),
+    path: '/quiz'
+  },
+  {
+    name: 'Flashcards',
+    image: require('./../../assets/images/flashcard.png'),
+    icon: require('./../../assets/images/layers.png'),
+    path: '/flashcards'
+  },
+  {
+    name: 'Question & Ans',
+    image: require('./../../assets/images/notes.png'),
+    icon: require('./../../assets/images/qa.png'),
+    path: '/questionAnswer'
+  }
+];
 
 export default function PracticeSection() {
   return (
-    <View>
+    <View style={{ padding: 10 }}>
       <Text style={{
-        fontFamily : 'outfit-bold',
-        fontSize : 20,
+        fontFamily: 'outfit-bold',
+        fontSize: 20,
+        marginBottom: 10
       }}>Practice Section</Text>
 
-      <View>
-        <FlatList 
-            data = {PracticeOption}
-            renderItem={({ item, index }) => (
-                <View key ={index}>
-                    <Image source={item?.image} style= {{
-                        width : 200,
-                        height: 200,
-                        maxHeight: 160,
-                        borderRadius: 15
-                    }}
-                    />
-
-                </View>
-            )}
-
-        />
-      </View>
-
-
-
-
-
+      <FlatList 
+        data={PraticeOption}
+        keyExtractor={(item, index) => index.toString()}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => (
+          <View style={{ marginRight: 15 }}>
+            <Image 
+              source={item.image} 
+              style={{
+                width: 200,
+                height: 160,
+                borderRadius: 15,
+                resizeMode: 'cover'
+              }} 
+            />
+          </View>
+        )}
+      />
     </View>
-  )
+  );
 }
